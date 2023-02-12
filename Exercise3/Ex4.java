fc25ef28522ec253c4c8f040abf88bd9132f9010
eak022 <eakeakkachai71180@gmail.com>
2023-02-13 05:45:51 +0700

    13-02

--- Exercise3/Ex4.java
+++ Exercise3/Ex4.java
@@ -7,26 +7,21 @@ System.out.print("EnterNumber1: ");
 int number1 = input.nextInt();
 System.out.print("EnterNumber2: ");
 int number2 = input.nextInt();
+    for(; number2 == number1; ){
+        System.out.print("Duplicate number, please enter the number2 again.: ");
+        number2 = input.nextInt();
+    }
 System.out.print("EnterNumber3: ");
 int number3 = input.nextInt();
-for( ; number1 == number2 ;){
-    System.out.print("Duplicate number, please enter the number1 again.: ");
-    number1 = input.nextInt();
-    System.out.print("Duplicate number, please enter the number2 again.: ");
-    number2 = input.nextInt();
-}
-    for( ; number2 == number3 ;){
-        System.out.print("Duplicate number, please enter the number2 again.: ");
-    number2 = input.nextInt();
-    System.out.print("Duplicate number, please enter the number3 again.: ");
-    number3 = input.nextInt();
-    }
-    for( ; number3 == number1 ;){
+    for(; number3 == number1 ;){
+        if(number3 == number1){
         System.out.print("Duplicate number, please enter the number3 again.: ");
-    number3 = input.nextInt();
-    System.out.print("Duplicate number, please enter the number1 again.: ");
-    number1 = input.nextInt();
+        number3 = input.nextInt();
+        }
+        for(;number3 == number2;){ System.out.print("Duplicate number, please enter the number3 again.: ");
+        number3 = input.nextInt();}
     }
+    
 
     if(number1 > number2){
          if (number1 > number2){
