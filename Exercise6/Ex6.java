ce4f574f60abe2f056cac1dc8746ecd142181d44
eak022 <121035260+eak022@users.noreply.github.com>
2023-03-27 17:23:34 +0700

    Add files via upload

--- Exercise6/Ex6.java
+++ Exercise6/Ex6.java
@@ -72,26 +72,75 @@ public class Ex6{
 
     }
     /*7isDividedByN() */
-    public void isDividedByN(int num, Scanner input){
+    public void isDividedByN(int n,int x, Scanner input){
+        String answers = "" ;
+        for (int i = 1; i <= n;i++){
+            if (i % x == 0 ){                                                           
+                    if( answers ==  ""){
+                        answers = answers + i;
+                    }
+                    else{
+                        answers = answers + "," + i ;            
+                    }
+                   
+                    
+                
+            }
+
+        }
+        System.out.println(" 1- " + n +" number divided by " + x + " fit is as follows " + answers );
+    }
+    
+    // 8 printStarAdvance()
+    public void printStarAdvance (int num, Scanner input){
+        String u = "";
+        String s = "*";
+        for( int i = 1;i < num; i++){                    
+            if( i == 1 ){            
+                System.out.println(s);                              
+            }
+            u = u + "_" ;
+            System.out.print(s);
+            System.out.print(u);
+            System.out.println(s);
+
+        }
 
     }
+        
+
 
     public static void main(String[] args){
         Ex6 ex = new Ex6();
         Scanner input = new Scanner(System.in);
-    //    System.out.println("Please, enter a number: ");
-    //     int num = input.nextInt();
-    System.out.println("Please, enter a number: ");
+
+        //1 printType1, 2 printType2, 5 ispositive, 6 isodd
+       System.out.println("Please, enter a number: ");
         int num = input.nextInt();
-        //ex.printType1(num); 
-        //ex.printType2(num);
+    //     ex.printType1(num); 
+    //     ex.printType2(num);
+    //     ex.ispositive(num, input);
+    //     ex.isodd(num, input);
+            ex.printStarAdvance(num, input);
+}
+     // 3checkvowel,4checkchar
         //  System.out.println("Please, enter a character: ");
         //  String text = input.next();
         //  char c = text.charAt(0);
          //ex.checkvowel(c);
          //ex.checkchar(c);
-        //  ex.ispositive(num, input);
-        ex.isodd(num, input);
-    }
+
+
+     /*7isDividedByN() */
+        // System.out.println("Please, enter a number: ");
+        // int n = input.nextInt();
+        // System.out.println("Please, enter a number: ");
+        // int x = input.nextInt();
+        //    ex.isDividedByN(n,x, input);
+
+
+       
+        
+       
    
 }
